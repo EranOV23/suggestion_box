c07dd1ca88e1460de923cbf5910161af52f116ac
EranOV23 <eran.ovadia123@gmail.com>
2016-07-08 17:36:50 +0300

    Update suggestions.js

--- suggestion_box/js/services/suggestions.js
+++ suggestion_box/js/services/suggestions.js
@@ -2,24 +2,24 @@ app.factory('suggestions',[function(){
 	var demoSuggestions = {
 		posts: [
 			{
-				title: 'Happy Hour every Sunday evening',
-				upvotes: 23,
-				comments: [],
-			},
-			{
 				title: 'Free pizza at club meetings',
 				upvotes: 15,
-				comments: [],
+				comments: ['good'],
+			},		
+			{
+				title: 'Happy Hour every Sunday evening',
+				upvotes: 23,
+				comments: ['bad'],
 			},
 			{
 				title: 'End all club emails with Laffy Taffy jokes',
 				upvotes: 9,
-				comments: [],
+				comments: ['fine'],
 			},
 
+			
 
-
-		]
+		] 
 	};
 	return demoSuggestions;
 }]);
